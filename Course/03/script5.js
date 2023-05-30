44c711649c625713e02b590f5a3b288a08013690
Alberto <betim009@gmail.com>
2023-05-30 14:43:33 -0300

    script5 03

---
+++ Course/03/script5.js
@@ -0,0 +1,16 @@
+// Arrow functions ( ) => { }
+
+const some = (num1, num2) => {
+    return num1 + num2;
+};
+
+console.log(some(5, 10)) // 15
+
+
+// Modelos com uma linha:
+// Não precisa de return
+// Não precisa de bigodes {}
+const someTwo = (num1, num2) =>  num1 + num2;
+
+
+console.log(someTwo(20, 20)) // 40

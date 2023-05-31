d9d409d8de88d271a968ed94c5f2a7bb393d3fa5
Alberto <betim009@gmail.com>
2023-05-31 18:58:27 -0300

    script3 01

--- Course/01/script3.js
+++ Course/01/script3.js
@@ -1,15 +1,33 @@
-// Operadores de comparação
+// 1. Operadores de comparação:
+console.log("1. Operadores de comparação: ") // ignore
 
-// Operador de Comparação	Descrição
-// ==	Igual a (valor)
-// ===	Estritamente igual a (valor e tipo)
-// !=	Diferente de
-// >	Maior que
-// >=	Maior ou igual que
-// <	Menor que
-// <=	Menor ou igual que
+const valorA = 10;
+const valorB = 20;
+const valorC = "Alberto";
+const valorD = "Creuza";
 
-// Estrutura If Else / Se Senão
+// Operador "==" e Igual a (valor):
+console.log("\nOperador '==' e Igual a (valor): ") // ignore
+let compare = valorA == valorB; 
+console.log(compare);
+
+// Operador Estritamente igual "===":
+console.log("Operador Estritamente igual '===':") // ignore
+compare = valorA === 10;
+console.log(compare);
+
+// Operador maior e menor:
+console.log("Operador maior e menor:") // ignore
+compare = valorA > valorB;
+console.log(compare);
+
+compare = valorA < valorB;
+console.log(compare);
+
+// 2. Estrutura If Else / Se Senão
+console.log('\n2. Estrutura If Else / Se Senão');
+
+// 2.1 Maior de idade
 const idade = 90;
 
 if (idade >= 18) {
@@ -18,8 +36,9 @@ if (idade >= 18) {
     console.log('Menor de idade.')
 }
 
+console.log('\n') // ignore
 
-
+// 2.2 Se pode votar:
 const idadeAlberto = 27;
 let canVote;
 
@@ -31,11 +50,22 @@ if (idadeAlberto >= 16) {
 
 console.log(canVote)
 
+console.log('\n') // ignore
 
-// Operador Ternário
-// <condição> ? <retorno caso condição seja verdadeira> : <retorno caso condição seja falsa>;
+// 3. Operador Ternário:
+console.log('3. Operador Ternário:')
 
 const idadePessoa = 15;
 
-const youCanVote = idadePessoa >= 16 ? "Pode Votar" : "Não Pode";
+// <condição> ? <retorno caso condição seja verdadeira> : <retorno caso condição seja falsa>;
+const youCanVote = idadePessoa >= 16 ? "Pode Votar!" : "Não Pode";
 console.log(youCanVote);
+
+// Operador de Comparação	Descrição
+// ==	Igual a (valor)
+// ===	Estritamente igual a (valor e tipo)
+// !=	Diferente de
+// >	Maior que
+// >=	Maior ou igual que
+// <	Menor que
+// <=	Menor ou igual que

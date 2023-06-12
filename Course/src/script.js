3c547c5933cac3ea9443c67fb9658108a8f84982
Alberto <betim009@gmail.com>
2023-06-12 17:02:42 -0300

    more example

--- Course/src/script.js
+++ Course/src/script.js
@@ -1,9 +1,18 @@
 const headers = ['Inicio', 'Contato', 'Sobre', 'Vendas'];
 
-for(let i = 0; i < headers.length; i+=1){
+for (let i = 0; i < headers.length; i += 1) {
     console.log(headers[i])
 };
 
-for(let i = headers.length - 1; i >= 0; i-=1){
+for (let i = headers.length - 1; i >= 0; i -= 1) {
     console.log(headers[i])
 };
+
+
+const string = 'cavalo';
+let newString = '';
+for (let i = string.length - 1; i >= 0; i -= 1) {
+    newString += string[i];
+};
+
+console.log(newString)

081e82562491dfb6c456bb2e726c1662e2ca7a67
Alberto <betim009@gmail.com>
2023-08-20 21:21:27 -0300

    fix: consoles

--- Course/Projetos/01_projeto_get_data/01/src/index.js
+++ Course/Projetos/01_projeto_get_data/01/src/index.js
@@ -8,19 +8,21 @@ const {
 
 // todos usuários
 const allUsers = getAllUsers();
+// console.log(allUsers)
 
 // busca por id
-const findUser = getUserById();
+const findUser = getUserById(31);
+// console.log(findUser)
 
 // cria usuário
-// const new_user_31 = createUser({ name: 'perry', email: 'perry@gmail.com', password: "pddd1ddd" });
+const new_user_31 = createUser({ name: 'perry', email: 'perry@gmail.com', password: "pddd1ddd" });
+// console.log(new_user_31)
 
 // altera usuario 
-const updateUser = updateUserById(30, { name: 'Alex', email: 'perry@email.com', password: '1234567' });
+const updateUser = updateUserById(31, { name: 'Alex', email: 'perry@email.com', password: '1234567' });
+// console.log(updateUser)
 
 // deleta usuario
-const deleteUser = deleteById(30);
+const deleteUser = deleteById(31);
+// console.log(deleteUser)
 
-// chamadas
-console.log(updateUser)
-console.log(deleteUser)

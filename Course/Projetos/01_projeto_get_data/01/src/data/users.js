275e8148dfb7baec6024094e77b2e2ad967cd7e0
Alberto Fernandes Couto <62117863+betim009@users.noreply.github.com>
2024-01-09 21:02:10 -0300

    Update users.js

--- Course/Projetos/01_projeto_get_data/01/src/data/users.js
+++ Course/Projetos/01_projeto_get_data/01/src/data/users.js
@@ -1,4 +1,4 @@
-const users = [
+export const users = [
     {
         id: 1,
         name: 'Alberto Fernandes Couto',
@@ -181,5 +181,5 @@ const users = [
     },
 ];
 
-module.exports = users;
+
 

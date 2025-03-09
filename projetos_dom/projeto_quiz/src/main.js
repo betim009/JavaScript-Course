938fca7c02133dea498b11185beeb2cb9c0eef24
unknown <betim009@gmail.com>
2025-03-09 19:24:01 -0300

    update

--- projetos_dom/projeto_quiz/src/main.js
+++ projetos_dom/projeto_quiz/src/main.js
@@ -1,33 +1,38 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import data from './api/data.json'
 
 const divQuestion = document.getElementById('div-questions')
 const divBar = document.getElementById('div-bar')
+const btnSend = document.getElementById('btn-send')
 
-const createQuestion = (questions) => {
-  return questions.map((e, i) => (
-    `<p>${i + 1} - ${e}</p>`
-  )).join(" ")
+const { results } = data
+let count = 0
+
+const createQuestions = () => {
+  divQuestion.innerHTML = ""
+  divQuestion.innerHTML += `
+    <div class="div-question">
+      <h3>${count + 1} - Pergunta:</h3>
+      <h4>${results[count].question}</h4>
+      <div>
+        ${createAlternatives(results[count].alternatives)}
+      </div>
+    </div>
+  `
 }
 
+const createAlternatives = (questions) => questions
+  .map((e, i) => (`<p>${i + 1} - ${e}</p>`)).join(" ")
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  const { results } = data
+  createQuestions();
+})
 
-  
-
-  results.map((e, i) => (
-    divQuestion.innerHTML += `
-      <div class="div-question">
-        <h3>${i + 1} - Pergunta:</h3>
-        <h4>${e.question}</h4>
-        <div>
-          ${createQuestion(e.alternatives)}
-        </div>
-      </div>
-    `
-  ))
+btnSend.addEventListener('click', () => {
+  count += 1
+  if (count + 1 > 5) {
+    return btnSend.disabled = true
+  }
+  createQuestions()
 })

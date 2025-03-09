2a99d7ac0415b0f91a0d0d849e2fcc8852c7970e
unknown <betim009@gmail.com>
2025-03-09 12:20:59 -0300

    update

--- projetos_dom/projeto_quiz/src/main.js
+++ projetos_dom/projeto_quiz/src/main.js
@@ -3,20 +3,25 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import data from './api/data.json'
 
-const divQuestion = document.getElementById('div-question')
+const divQuestion = document.getElementById('div-questions')
+const divBar = document.getElementById('div-bar')
 
 const createQuestion = (questions) => {
-  return questions.map(e => (
-    `<p>${e}</p>`
-  ))
+  return questions.map((e, i) => (
+    `<p>${i + 1} - ${e}</p>`
+  )).join(" ")
 }
 
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  data.results.map((e, i) => (
+  const { results } = data
+
+  
+
+  results.map((e, i) => (
     divQuestion.innerHTML += `
-      <div>
+      <div class="div-question">
         <h3>${i + 1} - Pergunta:</h3>
         <h4>${e.question}</h4>
         <div>

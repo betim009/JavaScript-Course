3307f4ca86871024498662fb1db419e616842115
unknown <betim009@gmail.com>
2025-04-10 11:31:42 -0300

    update

---
+++ javascript_dom/1_seletores/main.js
@@ -0,0 +1,8 @@
+const nome = document.getElementById('nome');
+const idade = document.getElementById('idade');
+const btn = document.getElementById('btn');
+const link = document.getElementById('link');
+
+// Inspecione o navegador e veja o resultado:
+console.log(nome, idade, btn, link)
+

5c85cb3349bdc309f24d95ebcdcc143e9f0f6955
unknown <betim009@gmail.com>
2025-05-16 17:53:51 -0300

    update

--- conteudo_html_css/ui/navBars/script.js
+++ conteudo_html_css/ui/navBars/script.js
@@ -1,9 +1,14 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+const myToggle = document.getElementById('my-toggle')
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  console.log(myToggle)
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+myToggle.addEventListener('click', myFunction)

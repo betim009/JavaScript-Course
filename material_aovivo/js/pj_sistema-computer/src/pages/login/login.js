9fd65a633f1e7c7f19cdeaf728f0d787e135429b
unknown <betim009@gmail.com>
2025-05-19 10:06:14 -0300

    update

--- material_aovivo/js/pj_sistema-computer/src/pages/login/login.js
+++ material_aovivo/js/pj_sistema-computer/src/pages/login/login.js
@@ -2,54 +2,25 @@ const email = document.getElementById("email");
 const password = document.getElementById("senha");
 const form = document.getElementById("loginForm");
 
-const usuarios = [
-  {
-    id: 1,
-    email: "admin@email.com",
-    senha: "admin",
-    telefone: "(11) 91234-5678",
-  },
-  {
-    id: 2,
-    email: "joao.souza@email.com",
-    senha: "minhasenha",
-    telefone: "(21) 99876-5432",
-  },
-  {
-    id: 3,
-    email: "maria.lima@email.com",
-    senha: "12345678",
-    telefone: "(31) 98765-4321",
-  },
-  {
-    id: 4,
-    email: "carlos.pereira@email.com",
-    senha: "segura123",
-    telefone: "(41) 99999-8888",
-  },
-  {
-    id: 5,
-    email: "juliana.oliveira@email.com",
-    senha: "teste@2025",
-    telefone: "(51) 91111-2222",
-  },
-];
-
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const emailValor = email.value;
   const passwordValor = password.value;
 
-  const verificLogin = usuarios.find(
-    (element) => element.email === emailValor && element.senha === passwordValor
-  );
-
-  if (verificLogin) {
+  const lsUsuario = JSON.parse(localStorage.getItem('lsUsuario'));
+  console.log(lsUsuario)
+  if (lsUsuario.email === emailValor && lsUsuario.password === passwordValor) {
     window.location.href = "../../../index.html"
+    localStorage.setItem('id-user', "1")
 
   } else {
-    alert("Não foi possível fazer login verifique email e senha")
+    alert('Login Inválido')
+    window.location.reload();
   }
+});
 
-   
+window.addEventListener('load', () => {
+  if (localStorage.getItem('id-user')) {
+    window.location.href = "../../../index.html"
+  };
 });
--- material_aovivo/js/pj_sistema-computer/src/pages/register/script.js
+++ material_aovivo/js/pj_sistema-computer/src/pages/register/script.js
@@ -1,9 +1,18 @@
 const divName = document.getElementById("name")
 const divEmail = document.getElementById("email")
+const divPassword = document.getElementById("password")
 const divTelefone = document.getElementById("telefone")
-const divComet = document.getElementById("comment")
 const divButton = document.getElementById("button")
 
-divButton.addEventListener("click", function(){
-    alert("click")
+divButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const usuario = {
+        nomeCompleto: divName.value,
+        email: divEmail.value,
+        password: divPassword.value,
+        telefone: divTelefone.value,
+    };
+
+    localStorage.setItem('lsUsuario', JSON.stringify(usuario));
 });

752b9b1eaad2d9cf254e26a9bc954b662ef39438
Alberto <betim009@gmail.com>
2025-06-05 13:51:17 -0300

    update

--- material_aovivo/js_dom/local_dor/script.js
+++ material_aovivo/js_dom/local_dor/script.js
@@ -2,18 +2,32 @@ const generoMusica = document.getElementById("genero-musica")
 const generoFilme = document.getElementById("genero-filme")
 const enviar = document.getElementById("enviar")
 
+// GUARDAR OS RESULTADOS
+let resultado = []
 
 // EVENTOS
 generoMusica.addEventListener("input", function(event) {
     console.log(event.target.checked)
     console.log(event.target.value)
+
+    if (event.target.checked == true) {
+        resultado.push(event.target.value)
+    } else {
+        resultado = resultado.filter(item => item != event.target.value)
+    }
 })
 
 generoFilme.addEventListener("input", function(event) {
     console.log(event.target.checked)
     console.log(event.target.value)
+
+    if (event.target.checked == true) {
+        resultado.push(event.target.value)
+    } else {
+        resultado = resultado.filter(item => item != event.target.value)
+    }
 })
 
 enviar.addEventListener("click", function() {
-    alert()
+    console.log(resultado)
 })

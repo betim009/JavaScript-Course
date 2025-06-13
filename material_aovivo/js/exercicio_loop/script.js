ca03d591a90e5e2893d5f1d6811cef884f053643
Alberto <betim009@gmail.com>
2025-06-13 10:03:19 -0300

    update

---
+++ material_aovivo/js/exercicio_loop/script.js
@@ -0,0 +1,40 @@
+const pokemons = require("./dados");
+
+// 1. Exibir abaixo com o mesmo resultado
+/*
+Num [1] Bulbasaur Tipo Grass
+Num [2] Ivysaur Tipo Grass
+...
+*/
+
+pokemons.forEach()
+
+for (let index = 0; index < pokemons.length; index++) {
+    const element = pokemons[index];
+}
+
+
+
+// 2. Filtrar os pokemons pelo tipo 1 por grass, fire, water
+const grass = pokemons.filter()
+const fire = pokemons.filter()
+const water = pokemons.filter()
+
+for (let index = 0; index < pokemons.length; index++) {
+    const element = pokemons[index];
+
+    if (element.type1 == "fire?") {
+
+    }
+}
+
+// 3. Filtrar/Encontrar os pokemons que nao tem 2 tipos. 
+// Filter (opcional)
+// For (opicional)
+
+// 4. Filtrar/Econtrar todos os pokemons do tipo 1 ou 2, que nao seja poison
+// Filter (opcional)
+// For (opicional)
+
+// 5. Fazer a exibição igual o primeiro exercicio, porem de tras para frente. 
+// apenas com for.

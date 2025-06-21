82f51a598c0952967f305266452b51f647c5d076
Alberto <betim009@gmail.com>
2025-06-21 15:51:18 -0300

    update

--- material_aovivo/js_dom/pj_sofascore/frontend/main.js
+++ material_aovivo/js_dom/pj_sofascore/frontend/main.js
@@ -1,7 +1,22 @@
 import { getBrasileirao } from "./services/getBrasileirao.js";
 
+const resultadoPartidas = document.getElementById("resultado-partidas");
+
 async function main() {
-    console.log(await getBrasileirao())
+    const api = await getBrasileirao();
+    console.log(api);
+
+    api.forEach(element => {
+        resultadoPartidas.innerHTML += `
+            <h6>${element.rodada} Rodada</h6>
+        `
+        element.jogos.forEach(jogo => {
+            resultadoPartidas.innerHTML += `
+                <p>${jogo.mandante} ${jogo.placar} ${jogo.visitante}</p>
+            `
+        })
+        resultadoPartidas.innerHTML += `<hr>`
+    });
 }
 
 main()

36037e9597b99ffaf05678d97a6dd2d3538193fd
Alberto <betim009@gmail.com>
2025-06-21 16:35:35 -0300

    update

---
+++ material_aovivo/js/exercicios_funcoes/enunciado.js
@@ -0,0 +1,150 @@
+// 1. Implemente a funcao media, recebe 2 parametros
+// Os parametros serao notas(numeros inteiros)
+// Deve retornar a soma dos numeros divido por dois. 
+function media(nota1, nota2) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(media(10, 7))
+console.log(media(8, 4))
+
+
+// 2. Parecido com a primeira funcao de media, so quer agora se
+// A media for maior que 5, retorne True
+// Senao retorne False
+function mediaMaiorQueCinco(nota1, nota2) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(mediaMaiorQueCinco(10, 7))
+console.log(mediaMaiorQueCinco(4, 3))
+
+
+// 3. Crie uma função chamada maiorNumero que recebe dois números como parâmetros
+// e retorna o maior deles.
+function maiorNumero(num1, num2) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(maiorNumero(10, 7))
+console.log(maiorNumero(4, 6))
+
+
+// 4. Crie uma função chamada parOuImpar que recebe um número como parâmetro
+// e retorna "Par" se o número for par, ou "Ímpar" se for ímpar.
+function parOuImpar(numero) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(parOuImpar(10))
+console.log(parOuImpar(7))
+
+
+// 5. Crie uma função chamada saudacao que recebe um nome como parâmetro
+// e retorna a mensagem "Olá, <nome>!".
+function saudacao(nome) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(saudacao("Maria"))
+console.log(saudacao("João"))
+
+
+// 6. Crie uma função chamada calcularAreaRetangulo que recebe dois parâmetros (base e altura)
+// e retorna a área do retângulo.
+function calcularAreaRetangulo(base, altura) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(calcularAreaRetangulo(10, 5))
+console.log(calcularAreaRetangulo(7, 3))
+
+
+// 7. Crie uma função chamada inverterString que recebe uma string como parâmetro
+// e retorna essa string invertida.
+function inverterString(str) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(inverterString("hello"))
+console.log(inverterString("world"))
+
+
+// 8. Crie uma função chamada menorNumeroArray que recebe um array de números
+// e retorna o menor valor encontrado no array.
+function menorNumeroArray(numeros) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(menorNumeroArray([10, 7, 3, 5]))
+console.log(menorNumeroArray([4, 6, 2, 8]))
+
+
+// 9. Crie uma função chamada dobro que recebe um número como parâmetro
+// e retorna o dobro desse número.
+function dobro(numero) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(dobro(10))
+console.log(dobro(7))
+
+
+// 10. Crie uma função chamada ehPositivo que recebe um número como parâmetro
+// e retorna true se o número for positivo, ou false caso contrário.
+function ehPositivo(numero) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(ehPositivo(10))
+console.log(ehPositivo(-7))
+
+
+// 11. Crie uma função chamada somaArray que recebe um array de números
+// e retorna a soma de todos os elementos do array.
+function somaArray(numeros) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(somaArray([10, 7, 3, 5]))
+console.log(somaArray([4, 6, 2, 8]))
+
+
+// 12. Crie uma função chamada contaVogais que recebe uma string como parâmetro
+// e retorna a quantidade de vogais (a, e, i, o, u) presentes na string.
+function contaVogais(str) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(contaVogais("hello"))
+console.log(contaVogais("world"))
+
+
+// 13. Crie uma função chamada multiplicaElementos que recebe um array de números e um número multiplicador
+// e retorna um novo array com todos os elementos multiplicados pelo multiplicador.
+function multiplicaElementos(numeros, multiplicador) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(multiplicaElementos([10, 7, 3, 5], 2))
+console.log(multiplicaElementos([4, 6, 2, 8], 3))
+
+
+// 14. Crie uma função chamada filtraPares que recebe um array de números
+// e retorna um novo array contendo apenas os números pares.
+function filtraPares(numeros) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(filtraPares([10, 7, 3, 5]))
+console.log(filtraPares([4, 6, 2, 8]))
+
+
+// 15. Crie uma função chamada concatenaStrings que recebe duas strings como parâmetros
+// e retorna uma nova string resultante da concatenação das duas.
+function concatenaStrings(str1, str2) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(concatenaStrings("hello", "world"))
+console.log(concatenaStrings("Maria", "João"))
---
+++ material_aovivo/js/exercicios_funcoes/ex_1.js
@@ -0,0 +1,6 @@
+function media(nota1, nota2) {
+    // IMPLEMENTAR ABAIXO
+}
+
+console.log(media(10, 7))
+console.log(media(8, 4))
